66ebe46dc39d60783bce765e3ea65cdf996bc9f7
tle09 <elena_larisa_t@yahoo.com>
2023-10-27 13:25:03 +0300

    ex1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,32 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const listEl = document.querySelector(".gallery");
+
+galleryItems.forEach((item) => {
+  const createlistEl = document.createElement("li");
+  createlistEl.classList.add("gallery__item");
+  createlistEl.innerHTML = ` <a class='gallery__link' href='${item.original}'>
+ <img class ='gallery__image'src='${item.preview}'
+ data-source='${item.original}' 
+        alt='${item.description}'/>
+        </a>`;
+
+  listEl.append(createlistEl);
+});
+listEl.addEventListener("click", openImageInLightbox);
+
+function openImageInLightbox(event) {
+  const clickedOn = event.target;
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  event.preventDefault();
+  basicLightbox
+    .create(
+      `<img width='1400' height ='900' src='${clickedOn.dataset.source}'/>`
+    )
+    .show();
+}
